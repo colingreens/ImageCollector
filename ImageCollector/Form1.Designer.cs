02bd32c7accd59562e3fd9f7f74b176a34797571
Colin <Colin@DESKTOP-CT5HNS1>
2018-12-17 18:20:53 -0600

    Add project files.

---
+++ ImageCollector/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace ImageCollector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBoxStream = new System.Windows.Forms.PictureBox();
+            this.comboBoxCams = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.button4 = new System.Windows.Forms.Button();
+            this.timeLapseIntervalLabel = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButtonTimeLapse = new System.Windows.Forms.RadioButton();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this._folderBrowserDialogMethod = new System.Windows.Forms.FolderBrowserDialog();
+            this.timeLapseTimer = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStream)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxStream
+            // 
+            this.pictureBoxStream.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxStream.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.pictureBoxStream.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxStream.Name = "pictureBoxStream";
+            this.pictureBoxStream.Size = new System.Drawing.Size(614, 492);
+            this.pictureBoxStream.TabIndex = 0;
+            this.pictureBoxStream.TabStop = false;
+            // 
+            // comboBoxCams
+            // 
+            this.comboBoxCams.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.comboBoxCams.Cursor = System.Windows.Forms.Cursors.Default;
+            this.comboBoxCams.FormattingEnabled = true;
+            this.comboBoxCams.Location = new System.Drawing.Point(7, 514);
+            this.comboBoxCams.Name = "comboBoxCams";
+            this.comboBoxCams.Size = new System.Drawing.Size(303, 21);
+            this.comboBoxCams.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(7, 542);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start/Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnStream_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.panel1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.timeLapseIntervalLabel);
+            this.groupBox1.Controls.Add(this.trackBar1);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.radioButtonTimeLapse);
+            this.groupBox1.Location = new System.Drawing.Point(633, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(160, 555);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Control Box";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 271);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Image Count";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(22, 158);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(102, 75);
+            this.panel1.TabIndex = 9;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(3, 49);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(53, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Hours";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(3, 26);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Minutes";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(4, 3);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Seconds";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(9, 499);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 23);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Make Video";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // timeLapseIntervalLabel
+            // 
+            this.timeLapseIntervalLabel.AutoSize = true;
+            this.timeLapseIntervalLabel.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLapseIntervalLabel.Location = new System.Drawing.Point(63, 68);
+            this.timeLapseIntervalLabel.Name = "timeLapseIntervalLabel";
+            this.timeLapseIntervalLabel.Size = new System.Drawing.Size(24, 27);
+            this.timeLapseIntervalLabel.TabIndex = 6;
+            this.timeLapseIntervalLabel.Text = "0";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(9, 107);
+            this.trackBar1.Maximum = 30;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(145, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Value = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(7, 530);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(145, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Record";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btnRecord_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Time Between Shots";
+            // 
+            // radioButtonTimeLapse
+            // 
+            this.radioButtonTimeLapse.AutoSize = true;
+            this.radioButtonTimeLapse.Location = new System.Drawing.Point(41, 19);
+            this.radioButtonTimeLapse.Name = "radioButtonTimeLapse";
+            this.radioButtonTimeLapse.Size = new System.Drawing.Size(73, 17);
+            this.radioButtonTimeLapse.TabIndex = 1;
+            this.radioButtonTimeLapse.TabStop = true;
+            this.radioButtonTimeLapse.Text = "Timelapse";
+            this.radioButtonTimeLapse.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(321, 542);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(305, 22);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Open Image Folder Save/Convert";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(323, 514);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(303, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // timeLapseTimer
+            // 
+            this.timeLapseTimer.Interval = 600000;
+            this.timeLapseTimer.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(63, 322);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 27);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 385);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Est Video Length";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(57, 420);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 27);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "0 s";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 572);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxCams);
+            this.Controls.Add(this.pictureBoxStream);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CamCapture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStream)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxStream;
+        private System.Windows.Forms.ComboBox comboBoxCams;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.FolderBrowserDialog _folderBrowserDialogMethod;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButtonTimeLapse;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label timeLapseIntervalLabel;
+        private System.Windows.Forms.Timer timeLapseTimer;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ImageCollector/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using Accord.Video.FFMPEG;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Drawing.Drawing2D;
+
+namespace ImageCollector
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection videoDevices;
+        private VideoCaptureDevice videoSource;
+        private string _myPicturesPath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+        private string _savePath;
+        private string _dateTimeStamp = DateTime.Now.ToString("yyyyMMddHHmmss");
+        private int _timeMagnitude = 60000;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in videoDevices)
+            {
+                comboBoxCams.Items.Add(device.Name);
+            }
+            videoSource = new VideoCaptureDevice();
+
+            if (comboBoxCams.Items.Count > 0)
+            {
+                comboBoxCams.SelectedIndex = 0;
+            }
+            textBox1.Text = _myPicturesPath;
+            _savePath = textBox1.Text;
+        }
+
+        private void btnStream_Click(object sender, EventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.Stop();
+                pictureBoxStream.Image = null;
+                pictureBoxStream.Invalidate();
+                timeLapseTimer.Stop();                
+            }
+            else
+            {
+                videoSource = new VideoCaptureDevice(videoDevices[comboBoxCams.SelectedIndex].MonikerString);
+                //set newframe event handler
+                videoSource.NewFrame += new NewFrameEventHandler(videoSource_NewFrame);
+                videoSource.Start();
+            }
+        }
+
+        private void videoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            pictureBoxStream.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBoxStream.Image = image;
+            SetPhotoCountVideoEstimate();
+        }
+
+        private void btnRecord_Click(object sender, EventArgs e)
+        {
+
+            pictureBoxStream.Image.Save(_savePath + "\\" + _dateTimeStamp + "imageTest.jpg", ImageFormat.Jpeg);
+
+            if (radioButtonTimeLapse.Checked)
+            {
+                timeLapseTimer.Start();
+            }            
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+
+            SeeWhichButtonIsChecked();
+            timeLapseIntervalLabel.Text = trackBar1.Value.ToString();
+
+            timeLapseTimer.Interval = trackBar1.Value * _timeMagnitude;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            _folderBrowserDialogMethod.SelectedPath = _myPicturesPath;
+            if (_folderBrowserDialogMethod.ShowDialog() == DialogResult.OK)
+            {
+               textBox1.Text = _folderBrowserDialogMethod.SelectedPath;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _savePath = textBox1.Text;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _dateTimeStamp = DateTime.Now.ToString("yyyyHHddHHmmss");
+            pictureBoxStream.Image.Save(_savePath + "\\" + _dateTimeStamp + "imageTest.jpg", ImageFormat.Jpeg);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (videoSource.IsRunning)
+            {
+                videoSource.Stop();
+            }
+        }
+
+        private void CompileFoldertoVid()
+        {
+            int width = 640;
+            int height = 480;
+
+            var imagesCollection = Directory.GetFiles(_savePath, "*Test.jpg", SearchOption.AllDirectories).ToList();
+            VideoFileWriter writer = new VideoFileWriter();
+
+            writer.Open(@"C:\Users\cbenson\Desktop\test.avi", width, height, 25, VideoCodec.MPEG4);
+
+            foreach (var frame in imagesCollection)
+            {
+                var bmp = new Bitmap(frame);
+                bmp = ResizeImage(bmp, width, height);
+                writer.WriteVideoFrame(bmp);
+            }
+            writer.Close();
+            MessageBox.Show("Images Converted to AVI");
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            CompileFoldertoVid();
+        }
+
+        private Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        private void SetPhotoCountVideoEstimate()
+        {
+            //int fileNumber = Directory.GetFiles(_savePath, "*.jpg", SearchOption.TopDirectoryOnly).Length;
+            //label3.Text = fileNumber.ToString();
+            //label5.Text = (fileNumber / 25).ToString() + " s";
+        }
+
+        private void SeeWhichButtonIsChecked()
+        {
+            if (radioButton1.Checked)
+            {
+                _timeMagnitude = 1000;
+            }
+            else if (radioButton2.Checked)
+            {
+                _timeMagnitude = 60000;
+            }
+            else if (radioButton3.Checked)
+            {
+                _timeMagnitude = 3600000;
+            }
+        }
+    }
+}
